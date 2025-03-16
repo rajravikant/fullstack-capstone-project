d31525b19712e355272262ce8ef27d638762d148
GeekyRavi <108944355+rajravikant@users.noreply.github.com>
2025-03-16 11:40:14 +0530

    Update App.js

--- giftlink-frontend/src/App.js
+++ giftlink-frontend/src/App.js
@@ -1,12 +1,10 @@
 import React from 'react';
-import { Routes, Route, useNavigate } from 'react-router-dom';
+import { Routes, Route} from 'react-router-dom';
 import MainPage from './components/MainPage/MainPage';
 import LoginPage from './components/LoginPage/LoginPage';
 import RegisterPage from './components/RegisterPage/RegisterPage';
 import DetailsPage from './components/DetailsPage/DetailsPage'
 import SearchPage from './components/SearchPage/SearchPage'
-// import Profile from './components/Profile/Profile'
-
 import Profile from './components/Profile/Profile'
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
